842f7c8c78354718fdcbfbc199a1126946de2b16
Phillip Luther <dev@phillipluther.com>
2023-02-05 09:11:35 -0800

    add (admittedly goofy) page transitions using svelte/transition

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,1 +1,6 @@
 export const prerender = true;
+
+export function load({ url }) {
+	const currentRoute = url.pathname;
+	return { currentRoute };
+}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,15 @@
 <script>
+	import { fade } from 'svelte/transition';
 	import Header from '$lib/components/Header.svelte';
 	import '$lib/styles/global.css';
+
+	export let data;
 </script>
 
 <Header />
 
-<main>
-	<slot />
-</main>
-
-<footer>Global Footer</footer>
+{#key data.currentRoute}
+	<main in:fade={{ duration: 99, delay: 99 }} out:fade={{ duration: 99 }}>
+		<slot />
+	</main>
+{/key}
